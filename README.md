88e7ddc12ef7da20063b52905847fd776c78cccb
David Barbosa <68976305+DavidBarbosadev@users.noreply.github.com>
2020-09-08 17:21:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# to-do-list-js
+ after learning the first concepts of JS like variables, functions, loops and DOM I managed to create this simple app .
---
+++ app.js
@@ -0,0 +1,59 @@
+
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+
+var todos = [
+    'Make Coffee',
+    'Drink Coffee',
+    'Refill'
+];
+
+
+function renderTodos(){
+    listElement.innerHTML= "";
+
+    for (todo of todos) {
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo);
+
+        var linkElement = document.createElement('a');
+
+        linkElement.setAttribute('href', '#');
+
+        var linkText = document.createTextNode('X');
+
+        linkElement.appendChild(linkText);
+        
+        todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
+
+
+        var pos = todos.indexOf(todo);
+        linkElement.setAttribute('onclick', 'deleteTodo(' + pos + ')')
+
+       
+        todoElement.appendChild(linkElement);
+        listElement.appendChild(todoElement);
+    }
+
+
+}
+renderTodos();
+
+
+function addTodo(){
+    var todoText = inputElement.value;
+    todos.push(todoText);
+    inputElement.value = ''
+    renderTodos();
+}
+
+
+buttonElement.onclick = addTodo;
+
+function deleteTodo(pos){
+    todos.splice(pos,1);
+    renderTodos();
+
+}
