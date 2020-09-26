15d7c2a5ba94b1f20e87c65c0422b5130ba9f4fa
David Barbosa <68976305+DavidBarbosadev@users.noreply.github.com>
2020-09-26 15:43:18 +0100

    saving on local storage

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 
+//working with DOM
 var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
-var todos = [
-    'Make Coffee',
-    'Drink Coffee',
-    'Refill'
-];
 
+var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
 
+// funtion to list all the values inside the array. 
 function renderTodos(){
     listElement.innerHTML= "";
 
     for (todo of todos) {
+        // using document.CreateElement to generate a li inside the ul to display the values that were inputed.
         var todoElement = document.createElement('li');
         var todoText = document.createTextNode(todo);
 
@@ -41,12 +40,13 @@ function renderTodos(){
 }
 renderTodos();
 
-
+// using the add todo funtion to colect the input value and store it inside the array. Then call the the funtion renderTodos() so it displays all the information
 function addTodo(){
     var todoText = inputElement.value;
     todos.push(todoText);
     inputElement.value = ''
     renderTodos();
+    saveToStorage();
 }
 
 
@@ -55,5 +55,11 @@ buttonElement.onclick = addTodo;
 function deleteTodo(pos){
     todos.splice(pos,1);
     renderTodos();
+    saveToStorage();
+
+}
+
+function saveToStorage(){
+    localStorage.setItem('list_todos', JSON.stringify(todos));
 
 }
